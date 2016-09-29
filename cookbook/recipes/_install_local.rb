8ac4dbcbb705b9a28b93aa95e3b4106d95cd4868
John Manero <jmanero@rapid7.com>
2016-09-29 16:29:19 -0400

    Add alternate installation methods to support local debugging
    * Add an enable flag to select service actions and gate notifications
    * Add an attribute to select the installation method (default github)

---
+++ cookbook/recipes/_install_local.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: turnstile
+# Recipe:: _install_local
+#
+# Copyright (C) 2016 Rapid7 LLC.
+#
+# Distributed under terms of the MIT License. All rights not explicitly granted
+# in the MIT license are reserved. See the included LICENSE file for more details.
+#
+
+Chef::Application.fatal!(
+  "Mount the Turnstile application at #{node['turnstile']['paths']['directory']}"
+) unless ::Dir.exist?(node['turnstile']['paths']['directory'])
+
+execute 'npm install' do
+  command '/usr/bin/npm install'
+  cwd node['turnstile']['paths']['directory']
+  environment 'HOME' => node['turnstile']['paths']['directory']
+end
