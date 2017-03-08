920c097c8aab70ac240ed6c0269bcc37fe2b1e2b
dgreene-r7 <dgreene@rapid7.com>
2017-03-08 11:02:57 -0500

    Split tests for Store base class out from the implementation class tests. Refactored tests for FileStore to reflect that change.

--- test/10-local-file-store.js
+++ test/10-local-file-store.js
@@ -1,55 +1,33 @@
 'use strict';
+const Path = require('path');
 
 require('./resource/config');
 
-const Errors = require('../lib/errors');
 const Local = require('../lib/provider/local');
 const expect = require('chai').expect;
 
-/*
- * Matches parameters in the keys.json resource
- */
-const fixture = {
-  key: '7bf9708aa51b7f7859d0e68b6b62b8ab',
-  secret: '6jzQ+NyqY7PwOFpipttvbp53baOI/bqGdn4DMc2ALN2v3+rcNYWz/T4r+jORJHBq'
+const db = {
+  path: Path.join(__dirname, 'resource/keys.json'),
+  signal: 'SIGHUP'
 };
 
-describe('lib/local/store', function storage() {
+describe('lib/local/file_store', function storage() {
   describe('Events', function events() {
     it('emits `update` after the data file is loaded', function behavior(done) {
-      new Local.FileStore(Config.get('local:db')).once('update', done);
-    });
+      const store = Local.Store(db);
 
-    it('responds to a reload SIGNAL', function behavior(done) {
-      new Local.FileStore(Config.get('local:db')).once('update', function updated() {
-        // This is only called after the DB is initially loaded.
-        this.once('update', done);
-
-        // This should trigger the inner 'update' handler, above.
-        process.emit('SIGHUP');
+      store.once('update', () => {
+        done();
       });
     });
 
     it('emits `error` if a data file can not be loaded', function behavior(done) {
-      new Local.FileStore({
+      Local.Store({
         path: 'foo/bar.json'
       }).once('error', function error(err) {
-        expect(err).to.be.an('error');
+        expect(err).to.be.an.instanceOf(Error);
         done();
       });
-    });
-  });
-
-  describe('Methods', function methods() {
-    const db = new Local.FileStore(Config.get('local:db'));
-
-    it('throws an error when `lookup` is called with an invalid key', function behavior() {
-      expect(() => db.lookup('bogus key')).to.throw(Errors.AuthorizationError);
-    });
-
-    it('`lookup` returns a secret when it is called with a valid key', function behavior() {
-      expect(() => db.lookup(fixture.key)).to.not.throw();
-      expect(db.lookup(fixture.key)).to.equal(fixture.secret);
     });
   });
 });
---
+++ test/10-store.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Path = require('path');
+
+const Errors = require('../lib/errors');
+const Local = require('../lib/provider/local');
+const expect = require('chai').expect;
+
+/*
+ * Mock local config object
+ */
+const db = {
+  path: Path.join(__dirname, 'resource/keys.json'),
+  signal: 'SIGHUP'
+};
+
+/*
+ * Matches parameters in the keys.json resource
+ */
+const fixture = {
+  key: '7bf9708aa51b7f7859d0e68b6b62b8ab',
+  secret: '6jzQ+NyqY7PwOFpipttvbp53baOI/bqGdn4DMc2ALN2v3+rcNYWz/T4r+jORJHBq'
+};
+
+describe('lib/local/store', function storage() {
+  describe('Events', function events() {
+    it('responds to a reload SIGNAL', function behavior(done) {
+      Local.Store(db).once('update', function updated() {
+        // This is only called after the DB is initially loaded.
+        this.once('update', done);
+
+        // This should trigger the inner 'update' handler, above.
+        process.emit('SIGHUP');
+      });
+    });
+  });
+
+  describe('Methods', function methods() {
+    const dbImpl = Local.Store(db);
+
+    it('throws an error when `lookup` is called with an invalid key', function behavior() {
+      expect(() => dbImpl.lookup('bogus key')).to.throw(Errors.AuthorizationError);
+    });
+
+    it('`lookup` returns a secret when it is called with a valid key', function behavior() {
+      expect(() => dbImpl.lookup(fixture.key)).to.not.throw();
+      expect(dbImpl.lookup(fixture.key)).to.equal(fixture.secret);
+    });
+  });
+});
