614de43202b3772bfaa30250eb884eb23f0f134f
davepgreene <davepgreene@gmail.com>
2017-04-06 15:59:56 -0400

    Deprecated unused test resource.

--- test/resource/http.js
+++ test/resource/http.js
@@ -1,17 +1,4 @@
 'use strict';
-/**
- * This check fails for assignment of values on an Object argument, and does not
- * allow for argument type-checking/default setters. */
-/* eslint-disable no-param-reassign */
-
-/**
- * This check is invalid. Functions defined outside of a class are still hoisted. */
-/* eslint-disable no-use-before-define */
-
-const Crypto = require('crypto');
-const EventEmitter = require('events').EventEmitter;
-const HTTP = require('http');
-const expect = require('chai').expect;
 
 /**
  * Testing stubs for HTTP.IncomingMessage and HTTP.ServerResponse
@@ -75,78 +62,6 @@ class ServerResponse extends require('stream').PassThrough {
   }
 }
 exports.ServerResponse = ServerResponse;
-
-/**
- * Stub server for client testing
- *
- * @param {Function}  ready
- * @return {HTTP.Server}
- */
-exports.server = function() {
-  const server = HTTP.createServer(function handler(req, res) {
-    const chunks = [];
-
-    req.on('data', (chunk) => chunks.push(chunk));
-    req.on('end', (chunk) => {
-      if (chunk) { chunks.push(chunk); }
-      req.body = Buffer.concat(chunks).toString('utf8');
-
-      if (handlers.hasOwnProperty(req.url)) {
-        return handlers[req.url](req, res);
-      }
-
-      const body = 'Not Found';
-
-      res.writeHead(404, {
-        'content-type': 'text/plain',
-        'content-length': Buffer.byteLength(body, 'utf8')
-      });
-
-      res.write(body, 'utf8');
-      res.end();
-    });
-  });
-
-  const handlers = server.handlers = {};
-
-  server.handle = function(handler) {
-    const path = '/' + Crypto.randomBytes(32).toString('hex');
-
-    if (handler instanceof Function) {
-      handlers[path] = handler;
-    } else if (handler instanceof Object) {
-      // Treat `handler` as a Fixture
-      handlers[path] = function _handler(req, res) {
-        expect(req.method).to.equal(handler.REQUEST.method);
-        expect(req.url).to.equal(path);
-        expect(req.headers).to.contain.keys({'content-type': handler.REQUEST.type});
-        expect(req.headers).to.contain.keys('content-length');
-        expect(req.body).to.equal(handler.REQUEST.body);
-
-        if (handler.RESPONSE.body) {
-          res.setHeader('content-length', Buffer.byteLength(handler.RESPONSE.body, 'utf8'));
-        }
-
-        res.setHeader('content-type', handler.RESPONSE.type);
-        res.writeHead(handler.RESPONSE.code);
-
-        if (handler.RESPONSE.body) {
-          res.write(handler.RESPONSE.body, 'utf8');
-        }
-
-        res.end();
-      };
-    }
-
-    return path;
-  };
-
-  server.start = function(ready) {
-    server.listen(0, '127.0.0.1', () => ready(server.address().port));
-  };
-
-  return server;
-};
 
 exports.bench = function(request, handler) {
   return new Promise((resolve, reject) => {
